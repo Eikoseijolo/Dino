9a8d38f917f5bd998d5d34e8d6ce146e33acf4cc
Eikoseijolo <124674906+Eikoseijolo@users.noreply.github.com>
2023-10-12 13:32:57 +0200

    Ajout des fichiers v1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="">
+    <title>test montagne</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+    <div id="mount1">
+        <img src="./mount1.png" alt="">
+    </div>
+    <div id="dino" class="dino-wrapper">
+        <div class="dino"><img src="./dino.png" alt=""></div>
+    </div>
+    <div id="mount2">
+        <img src="./mount2.png" alt="">
+    </div>
+</body>
+
+</html>
